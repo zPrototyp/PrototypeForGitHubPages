e67fc30f6905a8acf4f8f219132f194dd9278436
My <myglimsjo@gmail.com>
2025-08-15 15:11:34 +0200

    Base update

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/PrototypeForGitHubPages/',
+  base: 'https://zprototyp.github.io/PrototypeForGitHubPages/Frontend',
   server: {proxy: {'/api': 'https://zokubackend-atd8fhadcvaaf0he.swedencentral-01.azurewebsites.net'}}
 });
