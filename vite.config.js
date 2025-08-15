fe163ec4c501060017052f652552ab5a10dd2805
My <myglimsjo@gmail.com>
2025-08-15 15:52:53 +0200

    U

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://zprototyp.github.io/PrototypeForGitHubPages/Frontend/',
+  base: "https://zprototyp.github.io/PrototypeForGitHubPages/",
   server: {proxy: {'/api': 'https://zokubackend-atd8fhadcvaaf0he.swedencentral-01.azurewebsites.net'}}
 });
